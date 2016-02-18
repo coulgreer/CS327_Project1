394cd017b6dda31c151e926f321feac52f3da808
Coul Greer <cagreer@bsu.edu>
2016-02-18 11:23:48 -0500

    Allowed for user to select detect technique and enter string to send.

--- src/Sender.java
+++ src/Sender.java
@@ -1,12 +1,23 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Sender {
-
 	
-	private ArrayList<Character> payload = new ArrayList<Character>();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] payload;
+		final char FLAG = 'F';
+		final char ESC = 'E';
+		Scanner input = new Scanner(System.in);
+		System.out.println("Would you like to use byte count, byte stuffing, or bit stuffing");
+		String job = input.nextLine();
+		while (!job.equalsIgnoreCase("byte count") && !job.equalsIgnoreCase("byte stuffing")
+				&& !job.equalsIgnoreCase("bit stuffing")) {
+			System.out.println("Type one of the following to choose: \n'byte count'\n'byte stuffing'\n'bit stuffing'");
+			job = input.nextLine();
+		}
+		System.out.println("Enter a string to transmit");
+		String packet = input.nextLine();
+		payload = packet.split("(?!^)");
+		
 	}
 
 }
