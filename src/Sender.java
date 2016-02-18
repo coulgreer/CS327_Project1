ffda38fcc6df2e2ec1d26443ff89f8c773621547
Coul Greer <cagreer@bsu.edu>
2016-02-18 02:26:47 -0500

    Created a repository for project 1.

---
+++ src/Sender.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Sender {
+
+	
+	private ArrayList<Character> payload = new ArrayList<Character>();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
