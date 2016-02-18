a0cf8a752e0234e8b4fd1d93149f4ea3893d22f1
Coul Greer <cagreer@bsu.edu>
2016-02-18 12:08:12 -0500

    Added the ability to use byte stuffing.

--- src/Sender.java
+++ src/Sender.java
@@ -1,23 +1,41 @@
 import java.util.Scanner;
 
 public class Sender {
-	
+
 	public static void main(String[] args) {
 		String[] payload;
 		final char FLAG = 'F';
 		final char ESC = 'E';
 		Scanner input = new Scanner(System.in);
-		System.out.println("Would you like to use byte count, byte stuffing, or bit stuffing");
-		String job = input.nextLine();
-		while (!job.equalsIgnoreCase("byte count") && !job.equalsIgnoreCase("byte stuffing")
-				&& !job.equalsIgnoreCase("bit stuffing")) {
+		System.out.println("Choose either byte count, byte stuffing, or bit stuffing.");
+		String method = input.nextLine();
+		while (!method.equalsIgnoreCase("byte count") && !method.equalsIgnoreCase("byte stuffing")
+				&& !method.equalsIgnoreCase("bit stuffing")) {
 			System.out.println("Type one of the following to choose: \n'byte count'\n'byte stuffing'\n'bit stuffing'");
-			job = input.nextLine();
+			method = input.nextLine();
 		}
 		System.out.println("Enter a string to transmit");
-		String packet = input.nextLine();
-		payload = packet.split("(?!^)");
-		
+		String packetReceiver = "";
+		String packetSender = input.nextLine();
+		payload = packetSender.split("(?!^)");
+		if (method.equalsIgnoreCase("byte stuffing")) {
+			packetReceiver += "F";
+			for (String str : payload) {
+				switch (str) {
+				case "F":
+					packetReceiver += "EF";
+					break;
+				case "E":
+					packetReceiver += "EE";
+					break;
+				default:
+					packetReceiver += str;
+				}
+			}
+			packetReceiver += "F";
+		}
+		System.out.println("The resulting packet is: " + packetReceiver);
+
 	}
 
 }
